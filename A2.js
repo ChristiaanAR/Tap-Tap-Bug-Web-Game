e2fd83d65fc423690c1584f7abf5688d7de44a0e
ChristiaanAR <Christiaan@Rutten.ca>
2015-06-16 17:51:47 -0400

    added info bar and several functions

--- A2.js
+++ A2.js
@@ -1,21 +1,35 @@
 
+function setup() {
+    setHighScore(lvl1HS, 1);
+    document.getElementById("infoBar").style.display = 'none';
+}
+
+// variables
+var lvl1HS = 1;
+var lvl2HS = 2;
+var lvl, score, time, clock;
+var paused;
+
 function startGame() {
     /*
     * Get which lvl is selected
     * Hide all main menu elements
-    * Bring up game elements (un-hide them?)
+    * Bring up game elements
     * Start the game
     */
-    var lvl = 2;
+    lvl = 2;
     if (document.getElementById("lvl1").checked){
         lvl = 1;
     }
 
-    document.getElementById("main").style.visibility = 'hidden';
-}
+    document.getElementById("main").style.display = 'none';
+    document.getElementById("infoBar").style.display = 'block';
 
-var lvl1HS = 1;
-var lvl2HS = 2;
+    time = 0;
+    score = 0;
+    clock = setInterval(addTime, 1000); // timer
+    paused = false;
+}
 
 function setHighScore(HS, lvl) {
     if (lvl == 1) {
@@ -26,3 +40,35 @@ function setHighScore(HS, lvl) {
     }
     document.getElementById("HighScore").textContent = HS;
 }
+
+function addTime() {
+    time += 1;
+    document.getElementById("timer").textContent = time + " secs";
+    if (time == 60) {
+        // end game
+    }
+}
+
+function addScore(points) {
+    score += points;
+    if (lvl == 1 && score > lvl1HS) {
+        lvl1HS = score;
+    }
+    else if (score > lvl2HS) {
+        lvl2HS = score;
+    }
+}
+function pause() {
+    //stop bug movement, bring up pause menu.
+    if (paused == false) {
+        paused = true;
+        clearInterval(clock);
+        document.getElementById("pause").textContent = '|>';
+    }
+    else {
+        paused = false;
+        clock = setInterval(addTime, 1000);
+        document.getElementById("pause").textContent = "| |";
+    }
+}
+
